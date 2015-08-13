a2fd2b771427c6cb5883ac062d5434c40bfd2637
Caley Jack <caleyjack@me.com>
2015-08-13 00:09:57 -0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+console.log('Oh, this works?');
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImdsb2JhbC5jb2ZmZWUiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBQSxDQUFBLEdBQUEsQ0FBQSxpQkFBQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlUm9vdCI6Ii9zb3VyY2UvIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2cgJ09oLCB0aGlzIHdvcmtzPyciXX0=
